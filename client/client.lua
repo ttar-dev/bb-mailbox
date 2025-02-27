9db561388491c17fac889dc50a658815d0bbd184
K. Nattawut <ta.nattawut@icloud.com>
2025-02-27 14:35:38 +0700

    Add mailbox dependency and update NUI commands for mailbox functionality

--- client/client.lua
+++ client/client.lua
@@ -3,23 +3,44 @@ local function toggleNuiFrame(shouldShow)
   SendReactMessage('setVisible', shouldShow)
 end
 
-RegisterCommand('show-nui', function()
+-- dev cmd to show the NUI frame
+RegisterCommand('show-mailbox', function()
   toggleNuiFrame(true)
-  debugPrint('Show NUI frame')
+  debugPrint('>> Show Mailbox')
 end)
 
-RegisterNUICallback('hideFrame', function(_, cb)
+RegisterNUICallback('hide-mailbox', function(_, cb)
   toggleNuiFrame(false)
-  debugPrint('Hide NUI frame')
+  debugPrint('>> Hide Mailbox')
   cb({})
 end)
+
+local function queryMailbox(playerId, cb)
+  MySQL.Async.fetchAll('SELECT * FROM mailbox WHERE identifier = @player_id', {
+    ['@player_id'] = playerId
+  }, function(result)
+    if result then
+      cb(result)
+    else
+      debugPrint('No mailbox data found for player_id: ' .. playerId)
+      cb(nil)
+    end
+  end)
+end
 
 RegisterNUICallback('getClientData', function(data, cb)
   debugPrint('Data sent by React', json.encode(data))
 
--- Lets send back client coords to the React frame for use
+  -- Lets send back client coords to the React frame for use
   local curCoords = GetEntityCoords(PlayerPedId())
 
   local retData <const> = { x = curCoords.x, y = curCoords.y, z = curCoords.z }
-  cb(retData)
+
+  -- Query mailbox data from the database
+  queryMailbox(GetPlayerServerId(PlayerId()), function(mailboxData)
+    if mailboxData then
+      retData.mailboxData = mailboxData
+    end
+    cb(retData)
+  end)
 end)
