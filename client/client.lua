6f082956d0e69e8e531f100198e6918b95e332aa
K. Nattawut <ta.nattawut@icloud.com>
2025-02-28 01:36:58 +0700

    Add NUI callback to retrieve client coordinates and return mailbox data

--- client/client.lua
+++ client/client.lua
@@ -1,2 +1,14 @@
 local onInit = require('client/onInit')
 onInit()
+
+RegisterNUICallback('getClientData', function(data, cb)
+  debugPrint('Data sent by React', json.encode(data))
+
+  -- Lets send back client coords to the React frame for use
+  local curCoords = GetEntityCoords(PlayerPedId())
+
+  local retData <const> = { x = curCoords.x, y = curCoords.y, z = curCoords.z }
+
+  -- Query mailbox data from the database
+  cb(retData)
+end)
