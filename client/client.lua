7152a7f06b118f1dba8876d75a7da9461f624cda
K. Nattawut <ta.nattawut@icloud.com>
2025-02-28 02:12:33 +0700

    Implement mailbox toggle functionality and service for fetching messages

--- client/client.lua
+++ client/client.lua
@@ -1,3 +1,31 @@
+local function getAllMailboxMessagesService(playerId, cb)
+  MySQL.Async.fetchAll('SELECT * FROM mailbox WHERE identifier = @player_id', {
+    ['@player_id'] = playerId
+  }, function(result)
+    if result then
+      cb(result)
+    else
+      debugPrint('>> No mailbox data found for player_id: ' .. playerId)
+      cb(nil)
+    end
+  end)
+end
+
+local isMailboxOpen = false
+local function setToggleNuiFrame()
+  isMailboxOpen = not isMailboxOpen
+  SetNuiFocus(isMailboxOpen, isMailboxOpen)
+  debugPrint('>> Mailbox' .. (isMailboxOpen and ' opened' or ' closed'))
+  SendNUIMessage({
+    type = "ui",
+    status = isMailboxOpen
+  })
+end
+
+RegisterCommand('toggleMailbox', function()
+  toggleNuiFrame()
+end, false)
+
 RegisterNUICallback('getClientData', function(data, cb)
   debugPrint('>> Data sent by React', json.encode(data))
   local curCoords = GetEntityCoords(PlayerPedId())
@@ -34,34 +62,5 @@ RegisterNUICallback('handleOpenMailbox', function(data, cb)
   end)
 end)
 
-local function getAllMailboxMessagesService(playerId, cb)
-  MySQL.Async.fetchAll('SELECT * FROM mailbox WHERE identifier = @player_id', {
-    ['@player_id'] = playerId
-  }, function(result)
-    if result then
-      cb(result)
-    else
-      debugPrint('>> No mailbox data found for player_id: ' .. playerId)
-      cb(nil)
-    end
-  end)
-end
-
-local isMailboxOpen = false
-local function setToggleNuiFrame()
-  isMailboxOpen = not isMailboxOpen
-  SetNuiFocus(isMailboxOpen, isMailboxOpen)
-  debugPrint('>> Mailbox' .. (isMailboxOpen and ' opened' or ' closed'))
-  SendNUIMessage({
-    type = "ui",
-    status = isMailboxOpen
-  })
-end
-
-return setToggleNuiFrame
-
-RegisterCommand('toggleMailbox', function()
-  toggleNuiFrame()
-end, false)
 
 RegisterKeyMapping("toggleMailbox", "Toggle Mailbox", "keyboard", "F1")
