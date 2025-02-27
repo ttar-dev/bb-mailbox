0a4105f9e97b20ee39708235127e4f2f928de751
K. Nattawut <ta.nattawut@icloud.com>
2025-02-28 02:35:52 +0700

    Refactor mailbox UI handling: implement show/hide commands and streamline mailbox data querying

--- client/client.lua
+++ client/client.lua
@@ -1,66 +1,46 @@
-local function getAllMailboxMessagesService(playerId, cb)
+local function toggleNuiFrame(shouldShow)
+  SetNuiFocus(shouldShow, shouldShow)
+  SendReactMessage('setVisible', shouldShow)
+end
+
+-- dev cmd to show the NUI frame
+RegisterCommand('show-mailbox', function()
+  toggleNuiFrame(true)
+  debugPrint('>> Show Mailbox')
+end)
+
+RegisterNUICallback('hide-mailbox', function(_, cb)
+  toggleNuiFrame(false)
+  debugPrint('>> Hide Mailbox')
+  cb({})
+end)
+
+local function queryMailbox(playerId, cb)
   MySQL.Async.fetchAll('SELECT * FROM mailbox WHERE identifier = @player_id', {
     ['@player_id'] = playerId
   }, function(result)
     if result then
       cb(result)
     else
-      debugPrint('>> No mailbox data found for player_id: ' .. playerId)
+      debugPrint('No mailbox data found for player_id: ' .. playerId)
       cb(nil)
     end
   end)
 end
 
-local isMailboxOpen = false
-local function setToggleNuiFrame()
-  isMailboxOpen = not isMailboxOpen
-  SetNuiFocus(isMailboxOpen, isMailboxOpen)
-  debugPrint('>> Mailbox' .. (isMailboxOpen and ' opened' or ' closed'))
-  SendNUIMessage({
-    type = "ui",
-    status = isMailboxOpen
-  })
-end
-
-RegisterCommand('toggle-mailbox', function()
-  setToggleNuiFrame()
-end)
-
 RegisterNUICallback('getClientData', function(data, cb)
-  debugPrint('>> Data sent by React', json.encode(data))
+  debugPrint('Data sent by React', json.encode(data))
+
+  -- Lets send back client coords to the React frame for use
   local curCoords = GetEntityCoords(PlayerPedId())
+
   local retData <const> = { x = curCoords.x, y = curCoords.y, z = curCoords.z }
 
-  cb(retData)
-end)
-
-RegisterNUICallback('handleClaimReward', function(data, cb)
-  debugPrint('>> Button was pressed on the NUI')
-  -- Perform any action you want here
-  cb({})
-end)
-
-RegisterNUICallback('handleOpenMailbox', function(data, cb)
-  debugPrint('>> Data sent by React', json.encode(data))
-
-  local curCoords = GetEntityCoords(PlayerPedId())
-  local retData <const> = { x = curCoords.x, y = curCoords.y, z = curCoords.z }
-
-  getAllMailboxMessages(GetPlayerServerId(PlayerId()), function(mailboxData)
+  -- Query mailbox data from the database
+  queryMailbox(GetPlayerServerId(PlayerId()), function(mailboxData)
     if mailboxData then
       retData.mailboxData = mailboxData
     end
-
-    SendNUIMessage({
-      type = "renderMessages",
-      data = retData
-    })
-
-    setToggleNuiFrame()
-
     cb(retData)
   end)
 end)
-
-
-RegisterKeyMapping("setToggleNuiFrame", "Toggle Mailbox", "keyboard", "F1")
