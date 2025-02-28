9f887682183fdc683bc65fc65b732952b7323a51
K. Nattawut <ta.nattawut@icloud.com>
2025-02-28 15:21:34 +0700

    Refactor mailbox message retrieval: rename function for clarity and ensure MySQL library is loaded

--- client/client.lua
+++ client/client.lua
@@ -1,13 +1,14 @@
+-- Ensure MySQL library is loaded
+MySQL = exports['mysql-async']:getMySQL()
 
 -- functions
-local function getAllMailboxMessagesService(playerId, cb)
+local function getMailboxMessagesService(playerId, cb)
   MySQL.Async.fetchAll('SELECT * FROM mailbox WHERE identifier = @player_id', {
     ['@player_id'] = playerId
   }, function(result)
     debugPrint('>> Mailbox data found for player_id: ' .. playerId)
     debugPrint('>> Mailbox data found for result: ' .. result)
     if result then
-      
       cb(result)
     else
       debugPrint('>> No mailbox data found for player_id: ' .. playerId)
@@ -33,19 +34,19 @@ end)
 
 RegisterKeyMapping('toggle-mailbox', 'Toggle Mailbox', 'keyboard', 'F5')
 
--- RegisterKeyMapping('pm', 'Toggle Mailbox', 'keyboard', '0')
 RegisterNUICallback('handleClaimReward', function(data, cb)
   debugPrint('>> Button was pressed on the NUI')
   -- Perform any action you want here
   cb({})
 end)
+
 RegisterNUICallback('getMessages', function(data, cb)
   debugPrint('>> Data sent by React', json.encode(data))
 
   local curCoords = GetEntityCoords(PlayerPedId())
   local retData <const> = { x = curCoords.x, y = curCoords.y, z = curCoords.z }
 
-  getAllMailboxMessagesService(GetPlayerServerId(PlayerId()), function(mailboxData)
+  getMailboxMessagesService(GetPlayerServerId(PlayerId()), function(mailboxData)
     if mailboxData then
       retData.mailboxData = mailboxData
     end
