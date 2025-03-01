a2055c47eff3563b49e99d2c470942975083377c
K. Nattawut <ta.nattawut@icloud.com>
2025-03-01 20:27:07 +0700

    Update mailbox service to use discord_id and add functionality to insert new messages

--- server/server.lua
+++ server/server.lua
@@ -10,13 +10,33 @@ local function getDiscordIdentifier(source)
 end
 
 local function getMailboxMessagesService(playerId, cb)
-    MySQL.Async.fetchAll('SELECT * FROM mailbox WHERE identifier = @player_id', {
+    MySQL.Async.fetchAll('SELECT * FROM mailbox WHERE discord_id = @player_id', {
         ['@player_id'] = playerId
     }, function(result)
         if result then
             cb(result)
         else
             cb(nil)
+        end
+    end)
+end
+
+local function addMailboxMessageService(data, cb)
+    MySQL.Async.execute('INSERT INTO mailbox (identifier, discord_id, type, title, content, campaign_id, reward_name, reward_qty, is_ack) VALUES (@identifier, @discord_id, @type, @title, @content, @campaign_id, @reward_name, @reward_qty, @is_ack)', {
+        ['@identifier'] = data.identifier,
+        ['@discord_id'] = data.discord_id,
+        ['@type'] = data.type,
+        ['@title'] = data.title,
+        ['@content'] = data.content,
+        ['@campaign_id'] = data.campaign_id,
+        ['@reward_name'] = data.reward_name,
+        ['@reward_qty'] = data.reward_qty,
+        ['@is_ack'] = data.is_ack
+    }, function(rowsChanged)
+        if rowsChanged > 0 then
+            cb(true)
+        else
+            cb(false)
         end
     end)
 end
@@ -35,3 +55,37 @@ AddEventHandler('getMailboxMessages', function()
         TriggerClientEvent('receiveMailboxMessages', source, nil)
     end
 end)
+
+-- add a new message to the mailbox
+RegisterNetEvent('addMailboxMessage')
+AddEventHandler('addMailboxMessage', function(data)
+    local source = source
+    local discordIdentifier = getDiscordIdentifier(source)
+
+    if discordIdentifier then
+        addMailboxMessageService({
+            identifier = GetPlayerIdentifiers(source)[1],
+            discord_id = discordIdentifier,
+            type = data.type,
+            title = data.title,
+            content = data.content,
+            campaign_id = data.campaign_id,
+            reward_name = data.reward_name,
+            reward_qty = data.reward_qty,
+            is_ack = data.is_ack
+        }, function(success)
+            if success then
+                TriggerClientEvent('mailboxMessageResp', source, true)
+            else
+                TriggerClientEvent('mailboxMessageResp', source, false)
+            end
+        end)
+    else
+        TriggerClientEvent('mailboxMessageResp', source, false)
+    end
+end)
+
+-- Exports
+exports('getMailboxMessagesService', getMailboxMessagesService)
+exports('getDiscordIdentifier', getDiscordIdentifier)
+exports('addMailboxMessageService', addMailboxMessageService)
