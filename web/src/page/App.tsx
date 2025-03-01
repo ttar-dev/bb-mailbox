5142c0f2907753fecd063d8de6df430de19e645d
K. Nattawut <ta.nattawut@icloud.com>
2025-03-01 16:47:02 +0700

    Add MailboxContent component for displaying messages and rewards

--- web/src/page/App.tsx
+++ web/src/page/App.tsx
@@ -1,16 +1,12 @@
 import React, {useEffect, useMemo, useState} from "react";
 import {debugData} from "../utils/debugData";
-import {HiOutlineEnvelopeOpen} from "react-icons/hi2";
 import {fetchNui} from "../utils/fetchNui";
 import {useVisibility} from "../providers/VisibilityProvider";
-import {TfiReload} from "react-icons/tfi";
-import PageWrapper from "../components/PageWrapper";
-import {AnimatePresence} from "framer-motion";
-
-import {IoReload} from "react-icons/io5";
 import _ from "lodash";
 import MailboxHeader from "../components/MailboxHeader";
 import PaginationBar from "../components/Pagination";
+import MailboxContent from "./MailboxContent";
+
 debugData([
     {
         action: "setVisible",
@@ -74,201 +70,16 @@ const App: React.FC = () => {
             <div className="bg-[url(/assets/bg.png)] bg-no-repeat bg-center w-screen h-screen flex justify-center items-center flex-col -mt-16">
                 <div className="relative w-[1128px] h-[830px]">
                     <MailboxHeader />
-                    <div className="grid grid-cols-3 p-6 pt-10 mx-auto mt-[42px]">
-                        <div
-                            className={`transition-all duration-300 ${
-                                isMailOpenAnimation
-                                    ? "col-span-2"
-                                    : "col-span-3"
-                            }`}
-                        >
-                            <div className=" px-8 pt-14">
-                                <div className="bg-[#101010] relative w-full h-[560px] mt-10 rounded-[30px] p-5 pb-0">
-                                    <div className="item-List">
-                                        {getMessages.map(
-                                            (mail: {
-                                                id: number;
-                                                title: string;
-                                                time: string;
-                                                icon: string;
-                                            }) => (
-                                                <button
-                                                    key={mail.id}
-                                                    onClick={() => {
-                                                        setLoading(true);
-                                                        handleMailClick();
-                                                    }}
-                                                    className="bg-[linear-gradient(270deg,#2b2b2b_0%,#101010_100%),url(/assets/MaskGroup.png)] bg-cover bg-right bg-blend-color border-2 border-white w-full h-[88px] rounded-2xl mb-2 p-[6px] px-2 flex items-center"
-                                                >
-                                                    {/* Run number */}
-                                                    <div className="w-[71px] h-[71px] bg-[#1E1E1E] rounded-xl flex items-center justify-center">
-                                                        <p className="text-6xl text-white">
-                                                            1
-                                                        </p>
-                                                    </div>
-                                                    {/* Title */}
-                                                    <div className="ml-4">
-                                                        <div className="w-[155px] h-[27px] bg-[#1E1E1E] rounded-xl flex items-center justify-center">
-                                                            <p className="text-white font-noto">
-                                                                {mail.title}
-                                                            </p>
-                                                        </div>
-                                                        <p className="mt-2 text-white text-left">
-                                                            {mail.time}
-                                                        </p>
-                                                    </div>
-                                                    {/* Img */}
-                                                    <div className="w-[71px] h-[71px] bg-[#F4f5f6] rounded-xl flex items-center justify-center ml-auto">
-                                                        <img
-                                                            src={mail.icon}
-                                                            alt={mail.title}
-                                                            className="max-w-full max-h-full"
-                                                        />
-                                                    </div>
-                                                </button>
-                                            )
-                                        )}
-                                    </div>
-                                    {getMessages?.length === 0 && (
-                                        <div className="flex justify-center flex-col gap-3 items-center h-full">
-                                            <span className="text-white text-[4em] opacity-50">
-                                                <HiOutlineEnvelopeOpen />
-                                            </span>
-
-                                            <p className="text-white font-noto text-2xl font-bold opacity-50">
-                                                ไม่มีข้อความ
-                                            </p>
-                                            <button
-                                                className="flex items-center gap-3 bg-[#56c0fb] text-white font-noto text-xl px-4 py-2 rounded-3xl"
-                                                onClick={() => {
-                                                    // setLoading(true);
-                                                    // if (!isMailOpen) {
-                                                    //     setIsMailOpenAnimation(
-                                                    //         true
-                                                    //     );
-                                                    // }
-                                                    // setIsMailOpen(!isMailOpen);
-                                                    handleGetClientData();
-                                                }}
-                                            >
-                                                <TfiReload />
-                                                รีเฟรช
-                                            </button>
-                                        </div>
-                                    )}
-                                    <div
-                                        className={`absolute top-0 left-0 w-full h-full bg-[#101010] bg-opacity-50 backdrop-blur-2xl rounded-[30px] transition-opacity duration-500 ease-in-out ${
-                                            loading
-                                                ? "opacity-100"
-                                                : "opacity-0 pointer-events-none"
-                                        }`}
-                                    >
-                                        <div className="flex justify-center items-center w-full h-full">
-                                            <IoReload className="animate-spin text-white text-6xl" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <AnimatePresence>
-                            {isMailOpen && (
-                                <div className="col-span-1 transition-all duration-300">
-                                    <PageWrapper
-                                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                                        // @ts-ignore
-                                        onAnimationComplete={
-                                            handleAnimationComplete
-                                        }
-                                    >
-                                        <div className="pr-8 -mt-1 -ml-2 pt-[5em]">
-                                            <div className="bg-[#101010] relative w-full h-[560px] mt-5 rounded-[30px] p-5 pb-0 text-center">
-                                                <p className="py-1 mt-1 text-white font-noto text-2xl bg-gradient-to-r from-transparent via-[#4baaf8] to-transparent">
-                                                    ของรางวัลรายวัน
-                                                </p>
-                                                <div className="mt-6 flex items-center gap-3">
-                                                    {/* divide */}
-                                                    <div className="border w-1/4 rounded-full"></div>
-                                                    <div className="w-2/4 h-[41px] bg-[#1E1E1E] border-2 border-white rounded-3xl flex items-center justify-center">
-                                                        <p className="text-white font-noto text-base">
-                                                            รายละเอียดสินค้า
-                                                        </p>
-                                                    </div>
-                                                    <div className="border w-1/4 rounded-full"></div>
-                                                </div>
-
-                                                <div className="max-h-[12em] min-h-[8em] overflow-x-hidden overflow-y-auto">
-                                                    <p className="mt-3 text-left text-white font-noto">
-                                                        Lorem ipsum, dolor sit
-                                                        amet consectetur
-                                                        adipisicing elit. Cum,
-                                                        doloremque. Error
-                                                        temporibus, ab ea illo
-                                                        non quasi eaque
-                                                        accusamus eius modi
-                                                        quaerat itaque deserunt
-                                                        odit harum alias eum?
-                                                        Laudantium, deleniti.
-                                                        Lorem, ipsum dolor sit
-                                                        amet consectetur
-                                                        adipisicing elit. Nam
-                                                        rem non consectetur,
-                                                        laborum vel ducimus
-                                                        eaque atque dicta quasi
-                                                        velit ipsum cum? Laborum
-                                                        quo minima ratione,
-                                                        omnis rem mollitia sint!
-                                                    </p>
-                                                </div>
-
-                                                <div className="flex items-center gap-3">
-                                                    <div className="border w-1/4 rounded-full"></div>
-                                                    <div className="w-2/4 h-[41px] bg-[#1E1E1E] border-2 border-white rounded-3xl flex items-center justify-center">
-                                                        <p className="text-white font-noto text-xl">
-                                                            ของรางวัล
-                                                        </p>
-                                                    </div>
-                                                    <div className="border w-1/4 rounded-full"></div>
-                                                </div>
-                                                <div className="mt-4 bg-[linear-gradient(270deg,#2b2b2b_0%,#101010_100%),url(/assets/MaskGroup.png)] bg-cover bg-right bg-blend-color border-2 border-white w-full h-[88px] rounded-[20px] mb-2 p-[6px] px-2 flex items-center">
-                                                    <div className="w-[71px] h-[71px] bg-gradient-to-b from-[#A6F0FF] to-[#1181ED] rounded-xl flex items-center justify-center ">
-                                                        <img
-                                                            src={
-                                                                "/assets/Diamond.png"
-                                                            }
-                                                            className="max-w-full max-h-full"
-                                                        />
-                                                    </div>
-                                                    <div className="ml-4">
-                                                        <div className="w-[120px] h-[27px] border-2 border-white bg-[#1E1E1E] rounded-xl flex items-center justify-center">
-                                                            <p className="text-white font-noto">
-                                                                DIAMOND
-                                                            </p>
-                                                        </div>
-                                                        <p className="mt-2 text-white text-left">
-                                                            X100
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                                <div
-                                                    className={`absolute top-0 left-0 w-full h-full bg-[#101010] bg-opacity-50 backdrop-blur-2xl rounded-[30px] transition-opacity duration-500 ease-in-out ${
-                                                        loading
-                                                            ? "opacity-100"
-                                                            : "opacity-0 pointer-events-none"
-                                                    }`}
-                                                >
-                                                    <div className="flex justify-center items-center w-full h-full">
-                                                        <IoReload className="animate-spin text-white text-6xl" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </PageWrapper>
-                                </div>
-                            )}
-                        </AnimatePresence>
-                    </div>
-
+                    <MailboxContent
+                        getMessages={getMessages}
+                        isMailOpen={isMailOpen}
+                        isMailOpenAnimation={isMailOpenAnimation}
+                        loading={loading}
+                        handleMailClick={handleMailClick}
+                        handleGetClientData={handleGetClientData}
+                        handleAnimationComplete={handleAnimationComplete}
+                        setLoading={setLoading}
+                    />
                     <PaginationBar />
                 </div>
             </div>
