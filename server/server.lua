c8eae669a3172bc44fc8aa4264171531144f2714
K. Nattawut <ta.nattawut@icloud.com>
2025-03-02 11:50:26 +0700

    Refactor mailbox event handlers to improve message handling and service integration

--- server/server.lua
+++ server/server.lua
@@ -33,6 +33,25 @@ local function getMailboxMessagesService(identifier, discord_id, page, rowsPerPa
     end)
 end
 
+local function addMailboxItemsService(messages, cb)
+    local values = {}
+    for _, message in ipairs(messages) do
+        table.insert(values, string.format("('%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, %d)",
+            message.identifier, message.discord_id, message.type, message.title, message.content,
+            message.campaign_id, message.reward_name, message.reward_qty, message.is_ack))
+    end
+
+    local query = string.format("INSERT INTO mailbox (identifier, discord_id, type, title, content, campaign_id, reward_name, reward_qty, is_ack) VALUES %s", table.concat(values, ", "))
+
+    MySQL.Async.execute(query, {}, function(rowsChanged)
+        if rowsChanged > 0 then
+            cb(true)
+        else
+            cb(false)
+        end
+    end)
+end
+
 -- Handlers
 RegisterNetEvent('getMailboxMessages')
 AddEventHandler('getMailboxMessages', function(page)
@@ -84,70 +103,8 @@ AddEventHandler('addMailboxItem', function(data)
     end
 end)
 
-local function addMailboxItemsService(messages, cb)
-    local values = {}
-    for _, message in ipairs(messages) do
-        table.insert(values, string.format("('%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, %d)",
-            message.identifier, message.discord_id, message.type, message.title, message.content,
-            message.campaign_id, message.reward_name, message.reward_qty, message.is_ack))
-    end
-
-    local query = string.format("INSERT INTO mailbox (identifier, discord_id, type, title, content, campaign_id, reward_name, reward_qty, is_ack) VALUES %s", table.concat(values, ", "))
-
-    MySQL.Async.execute(query, {}, function(rowsChanged)
-        if rowsChanged > 0 then
-            cb(true)
-        else
-            cb(false)
-        end
-    end)
-end
-
--- Handlers
-RegisterNetEvent('getMailboxMessages')
-AddEventHandler('getMailboxMessages', function(page)
-    local source = source
-    local discordIdentifier = getDiscordIdentifier(source)
-    local rowsPerPage = 4
-
-    if discordIdentifier then
-        getMailboxMessagesService(discordIdentifier, page, rowsPerPage, function(mailboxData, maxPage, totalMessages)
-            TriggerClientEvent('receiveMailboxMessages', source, mailboxData, maxPage, totalMessages)
-        end)
-    else
-        TriggerClientEvent('receiveMailboxMessages', source, nil, 0, 0)
-    end
-end)
-
--- add a new message to the mailbox
-RegisterNetEvent('addMailboxItem')
-AddEventHandler('addMailboxItem', function(data)
-    local source = source
-    local discordIdentifier = getDiscordIdentifier(source)
-    
-    if discordIdentifier then
-        addMailboxItemService({
-            identifier = GetPlayerIdentifiers(source)[1],
-            discord_id = discordIdentifier,
-            type = data.type,
-            title = data.title,
-            content = data.content,
-            campaign_id = data.campaign_id,
-            reward_name = data.reward_name,
-            reward_qty = data.reward_qty,
-        }, function(success)
-            if success then
-                TriggerClientEvent('mailboxMessageResp', source, true)
-            else
-                TriggerClientEvent('mailboxMessageResp', source, false)
-            end
-        end)
-    else
-        TriggerClientEvent('mailboxMessageResp', source, false)
-    end
-end)
 
 -- Exports
 exports('getMailboxMsgSv', getMailboxMessagesService)
 exports('getDiscordIdSv', getDiscordIdentifier)
-exports('addMailboxMdgSv', addMailboxItemService)
+exports('addMailboxMdgSv', addMailboxItemsService)
