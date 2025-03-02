eb40f88ba6ef78ceb47d5a8c0f728ba59a027e91
K. Nattawut <ta.nattawut@icloud.com>
2025-03-02 19:07:45 +0700

    Update App to clear mail content only when the mailbox is closed

--- web/src/page/App.tsx
+++ web/src/page/App.tsx
@@ -136,7 +136,7 @@ const App: React.FC = () => {
     }, [isOpen, currentPage]);
 
     useEffect(() => {
-        setMailContent(null);
+        if (!isOpen) setMailContent(null);
     }, [isOpen]);
 
     useEffect(() => {
