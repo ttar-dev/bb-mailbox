b7352ca3454dbc9368452b10aad848ffb8c40866
K. Nattawut <ta.nattawut@icloud.com>
2025-03-02 22:46:29 +0700

    Simplify ESX object retrieval in server.lua for improved clarity

--- server/server.lua
+++ server/server.lua
@@ -1,9 +1,4 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) 
-    ESX = obj 
-    print('ESX object has been set') -- Debug print to check if ESX is set
-end)
+ESX = exports['es_extended']:getSharedObject()
 
 -- Functions
 local function getDiscordIdentifier(source)
