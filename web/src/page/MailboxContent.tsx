7dec7badff2a443e97e6304fe1e5cd2a86a0b932
K. Nattawut <ta.nattawut@icloud.com>
2025-03-02 11:22:47 +0700

    Refactor BlankContent component structure for improved layout and readability

--- web/src/page/MailboxContent.tsx
+++ web/src/page/MailboxContent.tsx
@@ -238,15 +238,17 @@ export default MailboxContent;
 
 const BlankContent: React.FC = () => {
     return (
-        <PageWrapper>
-            <div className="flex justify-center items-center w-full gap-4 h-full flex-col rounded-[30px]">
-                <span className="text-white text-[4em] opacity-50">
-                    <HiOutlineEnvelopeOpen />
-                </span>
-                <p className="text-white font-noto text-2xl font-bold opacity-50">
-                    ยังไม่ได้เปิดข้อความ
-                </p>
-            </div>
-        </PageWrapper>
+        <div className="h-full w-full flex justify-center items-center">
+            <PageWrapper>
+                <div className="flex justify-center items-center gap-4 flex-col">
+                    <span className="text-white text-[4em] opacity-50">
+                        <HiOutlineEnvelopeOpen />
+                    </span>
+                    <p className="text-white font-noto text-2xl font-bold opacity-50">
+                        ยังไม่ได้เปิดข้อความ
+                    </p>
+                </div>
+            </PageWrapper>
+        </div>
     );
 };
